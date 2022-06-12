209a872437f5345bba957b5773d310d6eb31e9fa
Estiven Mahecha henao <mahechahe@gmail.com>
2022-06-12 11:06:09 -0500

    Add .gitignore

--- index.js
+++ index.js
@@ -97,7 +97,8 @@ app.use((request, response) => {
 // .del
 // .put
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
   console.log(`Server runnin on port ${PORT}`)
 })
